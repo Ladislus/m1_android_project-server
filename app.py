53f986c29353e62703ae253552dd083d048f36ee
Ladislus <walcak.ladislas@gmail.com>
2020-11-19 23:32:00 +0100

    [MOD] Changed database file name

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 
