1ef311b99b675925f1581b9fa16f0ea95c7a326e
Ladislus <walcak.ladislas@gmail.com>
2020-11-20 20:42:30 +0100

    [MOD] Changed from double quotes to quotes

--- commands.py
+++ commands.py
@@ -20,24 +20,24 @@ def syncdb():
 def testmodel():
     """Peuplage test bd"""
 
-    u = User(_username="MamanGvomi", _password="AbricotSex")
-    u0 = User(_username="LHommeElastic", _password="JeSuisLadislas")
-    u1 = User(_username="WeeboMaster", _password="Tom")
+    u = User(_username='MamanGvomi', _password='AbricotSex')
+    u0 = User(_username='LHommeElastic', _password='JeSuisLadislas')
+    u1 = User(_username='WeeboMaster', _password='Tom')
     db.session.add_all([u, u0, u1])
     db.session.commit()
 
     c = Challenge(
-        _name="Chien mignon",
+        _name='Chien mignon',
         _type=True,
-        _theme="https://www.notretemps.com/cache/com_zoo_images/c9/nom-chiot_d7378d62ea795a70b121a517c938b895.jpg",
+        _theme='https://www.notretemps.com/cache/com_zoo_images/c9/nom-chiot_d7378d62ea795a70b121a517c938b895.jpg',
         _date=datetime.date(2020, 11, 5),
         _timer=30
     )
     db.session.add(c)
     db.session.commit()
 
-    d = Drawing(_link="https://proactivecreative.com/wp-content/uploads/2019/10/how-to-draw-a-dog--768x555.jpg")
-    d0 = Drawing(_link="https://i.pinimg.com/564x/0f/d9/98/0fd998799eceba85f138964def66085d.jpg")
+    d = Drawing(_link='https://proactivecreative.com/wp-content/uploads/2019/10/how-to-draw-a-dog--768x555.jpg')
+    d0 = Drawing(_link='https://i.pinimg.com/564x/0f/d9/98/0fd998799eceba85f138964def66085d.jpg')
     db.session.add_all([d, d0])
     db.session.commit()
 
