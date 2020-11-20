ca2e944be895bef3691624772ed257386134bbdd
Ladislus <walcak.ladislas@gmail.com>
2020-11-20 20:42:58 +0100

    [MOD] Changed from double quotes to simple quotes

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-
