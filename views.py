e19b6644107ccfe8b6e18432d49119eabbd3cbd3
Ladislus <walcak.ladislas@gmail.com>
2020-11-20 18:02:26 +0100

    [DEL] Removed former functions

--- views.py
+++ views.py
@@ -1,9 +1,9 @@
-from .app import app
-from .models import *
 from flask import jsonify
+
+from .app import app
 
 
 @app.route("/api/GetAllUser")
 def getAllUsers():
-    users = [user.jsonify() for user in User.query.all()]
+    users = [user.jsonify() for user in getAllUsers()]
     return jsonify(users)
