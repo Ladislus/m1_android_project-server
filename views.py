51c0ac5dacc047d08f3712917c6416fd78caa978
Ladislus <walcak.ladislas@gmail.com>
2020-11-21 01:00:40 +0100

    [ADD] Added getwhere()  - Added translation tables for operators and columns  - Added getwhere functions for all four entities  - renamed valid_request() to valid_key()  - Added missing call to valid_key() for get() functions

--- views.py
+++ views.py
@@ -1,8 +1,48 @@
 import os
-
+import operator
 from .app import *
 from .models import *
 from flask import jsonify, request
+
+#       ##########################
+#       #   Utility variables    #
+#       ##########################
+
+operators = {
+    'eq': operator.eq,
+    'lt': operator.lt,
+    'le': operator.le,
+    'gt': operator.gt,
+    'ge': operator.ge,
+    'ne': operator.ne,
+}
+
+columns = {
+    'user': {
+        'username': User._username,
+        'password': User._password,
+        'date': User._date
+    },
+    'drawing': {
+        'id': Drawing._id,
+        'link': Drawing._link,
+        'date': Drawing._date
+    },
+    'challenge': {
+        'id': Challenge._id,
+        'name': Challenge._name,
+        'type': Challenge._type,
+        'date': Challenge._date,
+        'timer': Challenge._timer
+    },
+    'participation': {
+        'u_id': Participation._user_id,
+        'd_id': Participation._drawing_id,
+        'c_id': Participation._challenge_id,
+        'is_creator': Participation._is_creator,
+        'votes': Participation._votes
+    }
+}
 
 
 #       #########################
@@ -28,7 +68,7 @@ def reply(resp):
 
 
 #   UTILITIES
-def valid_request(req):
+def valid_key(req):
     return req.headers.get('api_key') == os.environ.get('API_KEY')
 
 
@@ -39,7 +79,7 @@ def valid_request(req):
 #   FULL
 @app.route('/api/full', methods=['GET'])
 def api_full():
-    if not valid_request(request):
+    if not valid_key(request):
         return wrong_api_key()
     return reply(
         {
@@ -54,12 +94,17 @@ def api_full():
 #   USERS
 @app.route('/api/user/getall', methods=['GET'])
 def api_user_getall():
-    return reply([user.jsonify() for user in User.query.all()])
+    if not valid_key(request):
+        return wrong_api_key()
+    users = User.query.all()
+    if len(users) == 0:
+        return nothing_found()
+    return reply([user.jsonify() for user in users])
 
 
 @app.route('/api/user/get', methods=['GET'])
 def api_user_get():
-    if not valid_request(request):
+    if not valid_key(request):
         return wrong_api_key()
     if 'username' not in request.args:
         return missing_argument('username')
@@ -69,15 +114,36 @@ def api_user_get():
     return nothing_found()
 
 
+@app.route('/api/user/getwhere', methods=['POST'])
+def api_user_getwhere():
+    if not valid_key(request):
+        return wrong_api_key()
+    q = User.query
+    for field, params in request.json.items():
+        try:
+            q = q.filter(operators[params['operator']](columns['user'][field], request.json[field]['value']))
+        except KeyError:
+            pass  # Ignore unknown fields or operators
+    users = q.all()
+    if len(users) == 0:
+        return nothing_found()
+    return reply([user.jsonify() for user in users])
+
+
 #   DRAWINGS
 @app.route('/api/drawing/getall', methods=['GET'])
 def api_drawing_getall():
-    return reply([drawing.jsonify() for drawing in Drawing.query.all()])
+    if not valid_key(request):
+        return wrong_api_key()
+    drawings = Drawing.query.all()
+    if len(drawings) == 0:
+        return nothing_found()
+    return reply([drawing.jsonify() for drawing in drawings])
 
 
 @app.route('/api/drawing/get', methods=['GET'])
 def api_drawing_get():
-    if not valid_request(request):
+    if not valid_key(request):
         return wrong_api_key()
     if 'id' not in request.args:
         return missing_argument('id')
@@ -87,15 +153,36 @@ def api_drawing_get():
     return nothing_found()
 
 
+@app.route('/api/drawing/getwhere', methods=['POST'])
+def api_drawing_getwhere():
+    if not valid_key(request):
+        return wrong_api_key()
+    q = Drawing.query
+    for field, params in request.json.items():
+        try:
+            q = q.filter(operators[params['operator']](columns['drawing'][field], request.json[field]['value']))
+        except KeyError:
+            pass  # Ignore unknown fields or operators
+    drawings = q.all()
+    if len(drawings) == 0:
+        return nothing_found()
+    return reply([drawing.jsonify() for drawing in drawings])
+
+
 #   CHALLENGES
 @app.route('/api/challenge/getall', methods=['GET'])
 def api_challenge_getall():
-    return jsonify([challenge.jsonify() for challenge in Challenge.query.all()]), 200
+    if not valid_key(request):
+        return wrong_api_key()
+    challenges = Challenge.query.all()
+    if len(challenges) == 0:
+        return nothing_found()
+    return jsonify([challenge.jsonify() for challenge in challenges]), 200
 
 
 @app.route('/api/challenge/get', methods=['GET'])
 def api_challenge_get():
-    if not valid_request(request):
+    if not valid_key(request):
         return wrong_api_key()
     if 'id' not in request.args:
         return missing_argument('id')
@@ -105,15 +192,36 @@ def api_challenge_get():
     return nothing_found()
 
 
+@app.route('/api/challenge/getwhere', methods=['POST'])
+def api_challenge_getwhere():
+    if not valid_key(request):
+        return wrong_api_key()
+    q = Challenge.query
+    for field, params in request.json.items():
+        try:
+            q = q.filter(operators[params['operator']](columns['challenge'][field], request.json[field]['value']))
+        except KeyError:
+            pass  # Ignore unknown fields or operators
+    challenges = q.all()
+    if len(challenges) == 0:
+        return nothing_found()
+    return reply([challenge.jsonify() for challenge in challenges])
+
+
 #   PARTICIPATIONS
 @app.route('/api/participation/getall', methods=['GET'])
 def api_participation_getall():
-    return jsonify([participation.jsonify() for participation in Participation.query.all()]), 200
+    if not valid_key(request):
+        return wrong_api_key()
+    participations = Participation.query.all()
+    if len(participations) == 0:
+        return nothing_found()
+    return jsonify([participation.jsonify() for participation in participations]), 200
 
 
 @app.route('/api/participation/get', methods=['GET'])
 def api_participation_get():
-    if not valid_request(request):
+    if not valid_key(request):
         return wrong_api_key()
     if 'u_id' not in request.args:
         return missing_argument('u_id')
@@ -125,3 +233,20 @@ def api_participation_get():
     if c is not None:
         return reply(c.jsonify())
     return nothing_found()
+
+
+@app.route('/api/participation/getwhere', methods=['POST'])
+def api_participation_getwhere():
+    if not valid_key(request):
+        return wrong_api_key()
+    q = Participation.query
+    for field, params in request.json.items():
+        try:
+            q = q.filter(operators[params['operator']](columns['participation'][field], request.json[field]['value']))
+        except KeyError:
+            pass
+    print(q)
+    participations = q.all()
+    if len(participations) == 0:
+        return nothing_found()
+    return reply([participation.jsonify() for participation in participations])
