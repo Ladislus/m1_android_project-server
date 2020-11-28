9c51b0d6f4149b8ce3b6150ace8f9948f2ca40c0
Ladislus <walcak.ladislas@gmail.com>
2020-11-28 01:54:51 +0100

    [ADD] Added relation between User & Participation (_votes, _voters)

--- models.py
+++ models.py
@@ -11,6 +11,16 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.execute('PRAGMA foreign_keys=ON')
     cursor.close()
 
+participation_user = db.Table('PARTICIPATION_TO_USER', db.Model.metadata,
+        db.Column('u_id', db.String(255), ForeignKey('USER.u_username'), primary_key=True),
+        db.Column('p_u_id', db.Integer, primary_key=True),
+        db.Column('p_d_id', db.Integer, primary_key=True),
+        db.Column('p_c_id', db.Integer, primary_key=True),
+        db.ForeignKeyConstraint(
+            ('p_u_id', 'p_d_id', 'p_c_id'),
+            ('PARTICIPATION.p_u_id', 'PARTICIPATION.p_d_id', 'PARTICIPATION.p_c_id')
+        ),
+    )
 
 #############
 #   USER    #
@@ -23,7 +33,9 @@ class User(db.Model):
     _date = db.Column('u_date', db.DateTime, nullable=False)
     _salt = db.Column('u_salt', db.String(255), nullable=False)
 
-    _participations = relationship('Participation', back_populates='_user', cascade="delete")
+    _participations = relationship('Participation', back_populates='_user', cascade='delete')
+
+    _votes = relationship('Participation', secondary=participation_user, back_populates='_voters', cascade='delete')
 
     def jsonify(self):
         return\
@@ -44,7 +56,7 @@ class Drawing(db.Model):
     _link = db.Column('d_link', db.String(255), nullable=False, unique=True)
     _date = db.Column('d_date', db.DateTime, nullable=False)
 
-    _participation = relationship('Participation', back_populates='_drawing', cascade="delete", uselist=False)
+    _participation = relationship('Participation', back_populates='_drawing', cascade='delete', uselist=False)
 
     def jsonify(self):
         return\
@@ -69,7 +81,7 @@ class Challenge(db.Model):
     _date = db.Column('c_duration', db.DateTime, nullable=False)
     _timer = db.Column('c_timer', db.Integer, nullable=False)
 
-    _participations = relationship('Participation', back_populates='_challenge', cascade="delete")
+    _participations = relationship('Participation', back_populates='_challenge', cascade='delete')
 
     def jsonify(self):
         return\
@@ -99,6 +111,8 @@ class Participation(db.Model):
     _challenge = relationship('Challenge', back_populates='_participations')
     _is_creator = db.Column('p_is_creator', db.Boolean, nullable=False)
     _votes = db.Column('p_votes', db.Integer, nullable=False, default=0)
+
+    _voters = db.relationship('User', secondary=participation_user, back_populates='_votes', cascade='delete') 
 
     def jsonify(self):
         return\
